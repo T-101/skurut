c9025238fe679b76ff8cb667d469ac4a551888f4
T-101 <antti.rummukainen@gmail.com>
2025-01-20 23:19:41 +0200

    use a tad slower subscription

--- app/mqtt_service/apps.py
+++ app/mqtt_service/apps.py
@@ -10,7 +10,7 @@ class MqttServiceConfig(AppConfig):
     def ready(self):
         broker_url = "mqtt.hsl.fi"
         broker_port = 1883
-        topic = "/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/4/#"
+        topic = "/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/3/#"
         # topic = "/hfp/v2/journey/ongoing/vp/tram/+/+/+/+/+/+/+/2/#"  # slower, good for developing
 
         mqtt_client = MQTTClient(broker_url, broker_port, topic)
